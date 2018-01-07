28c47a25fc880937fb8e057e2b4a68ae56927aa5
Marcin Nabiałek <dev@nabialek.org>
2018-01-07 18:42:55 +0100

    Add Version class with tests

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -2,11 +2,89 @@
 
 namespace Mnabialek\LaravelVersion\Tests;
 
+use Illuminate\Contracts\Foundation\Application;
+use Mockery;
+use Mnabialek\LaravelVersion\Version;
+
 class VersionTest extends UnitTestCase
 {
-    /** @test */
-    public function it_asserts_true()
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @var Version
+     */
+    protected $version;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        $this->app = Mockery::mock(Application::class);
+        $this->version = new Version($this->app);
+    }
+
+    /** @test */
+    public function it_confirms_when_application_is_laravel()
+    {
+        $this->app->shouldReceive('version')->withNoArgs()->once()->andReturn('5.5.28');
+        $this->assertTrue($this->version->isLaravel());
+
+        $this->app->shouldReceive('version')->withNoArgs()->once()->andReturn('Lumen (5.5.2) (Laravel Components 5.5.*)');
+        $this->assertFalse($this->version->isLaravel());
+    }
+
+    /** @test */
+    public function it_confirms_when_application_is_lumen()
+    {
+        $this->app->shouldReceive('version')->withNoArgs()->once()->andReturn('Lumen (5.5.2) (Laravel Components 5.5.*)');
+        $this->assertTrue($this->version->isLumen());
+
+        $this->app->shouldReceive('version')->withNoArgs()->once()->andReturn('5.5.28');
+        $this->assertFalse($this->version->isLumen());
+    }
+
+    /** @test */
+    public function it_returns_full_version_string()
+    {
+        $version = 'Lumen 23238 test version';
+        $this->app->shouldReceive('version')->withNoArgs()->once()->andReturn($version);
+        $this->assertSame($version, $this->version->full());
+    }
+
+    /** @test */
+    public function it_returns_valid_application_version_for_laravel()
+    {
+        $version = '5.5.28';
+        $this->app->shouldReceive('version')->withNoArgs()->twice()->andReturn($version);
+        $this->assertSame($version, $this->version->get());
+    }
+
+    /** @test */
+    public function it_returns_valid_application_version_for_lumen()
+    {
+        $version = 'Lumen (5.5.2) (Laravel Components 5.5.*)';
+        $this->app->shouldReceive('version')->withNoArgs()->twice()->andReturn($version);
+        $this->assertSame('5.5.2', $this->version->get());
+    }
+
+    /** @test */
+    public function it_confirms_when_application_is_minimum_at_given_version_for_laravel()
+    {
+        $version = '5.5.28';
+        $this->app->shouldReceive('version')->withNoArgs()->times(6)->andReturn($version);
+        $this->assertTrue($this->version->min('5.5.27'));
+        $this->assertTrue($this->version->min('5.5.28'));
+        $this->assertFalse($this->version->min('5.5.29'));
+    }
+
+    /** @test */
+    public function it_confirms_when_application_is_minimum_at_given_version_for_lumen()
+    {
+        $version = 'Lumen (5.5.28) (Laravel Components 5.5.*)';
+        $this->app->shouldReceive('version')->withNoArgs()->times(6)->andReturn($version);
+        $this->assertTrue($this->version->min('5.5.27'));
+        $this->assertTrue($this->version->min('5.5.28'));
+        $this->assertFalse($this->version->min('5.5.29'));
     }
 }
